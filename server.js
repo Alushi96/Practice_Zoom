1e0c84a2a14766fa682af8fd5c91ee18fd6e0406
The Mechanic <jaber4132@gmail.com>
2020-07-08 22:53:32 -0400

    Added json thing

--- server.js
+++ server.js
@@ -9,9 +9,13 @@ app.use(express.json());
 
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
-    console.log(req);
-    console.log(res);
 });
+
+app.get("/code=:id", function(req,res) {
+  console.log("req");
+  console.log(req.params);
+  return res.json(req.params);
+})
 
 app.listen(port, function() {
   console.log(`App Server is listening on PORT: ${port}`);
